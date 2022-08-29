bd0c52923eeee90f5d36435d04f845984b6cf316
LGD <OxldAnE@qq.com>
2022-08-29 17:29:27 +0800

    17

---
+++ Vue/vue2/router/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>你好啊</div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Vue/vue2/router/src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <form @submit.prevent="postData">
+      <input type="text" placeholder="用户名">
+      <input type="password" placeholder="密码">
+      <button>提交</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name   : 'Login',
+  methods: {
+    postData () {
+
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
