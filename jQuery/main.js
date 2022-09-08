19bbf1146470aaa01ca22302b6bfa995c5656e09
LGD <OxldAnE@qq.com>
2022-09-08 22:38:36 +0800

    11

--- jQuery/main.js
+++ jQuery/main.js
@@ -1,8 +1,3 @@
 $(function () {
-  let arr = ['red', 'blue']
 
-  // arr.each(function (index, item) {console.log(item)})
-  $.each([{a : 1, b : 2}], function (index, item) {
-    console.log(item)
-  })
 })
